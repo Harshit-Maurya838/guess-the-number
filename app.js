92a8a6923f68ba9aa789e57c18a46d173ecc9aad
Harshit-Maurya <44harshitmaurya@gmail.com>
2024-07-03 21:26:43 +0530

    fixes problem of negative score and changed score range

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Extracting component from html 
 
 let inputNum = document.getElementById("guess");
-const randomNum = Math.floor(Math.random() * 100); // in this line random number will generate
+const randomNum = Math.floor(Math.random() * 10); // in this line random number will generate
 const displayScore = document.querySelector("#Score");
 const displayMessage = document.querySelector(".message");
-let score = 100; // starting Score
+let score = 10; // starting Score
 
 // This Fuction Calls Out When Check Button Click
 const checkTheNumber = () => {
-    if (inputNum.valueAsNumber >= 0 && inputNum.valueAsNumber <= 100) {
+    if(score !== 0){if (inputNum.valueAsNumber >= 0 && inputNum.valueAsNumber <= 10) {
         if (inputNum.valueAsNumber === randomNum) {
             displayMessage.innerText = `Hurray! You got The Number ${randomNum} an you Score is ${score}`;
             displayScore.innerText = `${score}`;
         } else {
             score--;
-            if (inputNum.valueAsNumber < randomNum && Math.abs(inputNum.valueAsNumber - randomNum) <= 5) {
+            if (inputNum.valueAsNumber < randomNum && Math.abs(inputNum.valueAsNumber - randomNum) <= 2) {
                 displayMessage.innerText = "You are very close to the Number";
                 console.log(inputNum.valueAsNumber);
                 displayScore.innerText = `${score}`;
@@ -35,6 +35,11 @@ const checkTheNumber = () => {
         displayMessage.innerText = "Please enter your number between 0 to 100 and then check your answer!";
         console.log(inputNum.valueAsNumber);
         inputNum.value = "";
+    }}else{
+        displayMessage.innerText = "Ohh you fail the challenge!"
+        setTimeout(() =>{
+            location.reload();
+        }, 5000);
     }
 }
 
