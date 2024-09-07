80862bdd7018f5e2f4ab4b736a90b22d63ca25d7
Harshit-Maurya <44harshitmaurya@gmail.com>
2024-09-07 13:29:51 +0530

    decreases the very close difference between number

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Extracting component from html 
+// Extracting component from html
 
 let inputNum = document.getElementById("guess");
 const randomNum = Math.floor(Math.random() * 100); // in this line random number will generate
@@ -8,46 +8,53 @@ let score = 10; // starting Score
 
 // This Fuction Calls Out When Check Button Click
 const checkTheNumber = () => {
-    if(score !== 0){if (inputNum.valueAsNumber >= 0 && inputNum.valueAsNumber <= 100) {
-        if (inputNum.valueAsNumber === randomNum) {
-            displayMessage.innerText = `Hurray! You got The Number ${randomNum} and your Score is ${score}`;
-            displayScore.innerText = `${score}`;
-            setTimeout(() =>{location.reload()}, 5000);
-        } else {
-            score--;
-            if (inputNum.valueAsNumber < randomNum && Math.abs(inputNum.valueAsNumber - randomNum) <= 5) {
-                displayMessage.innerText = "You are very close to the Number";
-                console.log(inputNum.valueAsNumber);
-                displayScore.innerText = `${score}`;
-                inputNum.value = "";
-            } else if (inputNum.valueAsNumber < randomNum) {
-                displayMessage.innerText = "This Number comes before the Guess Number";
-                console.log(inputNum.valueAsNumber);
-                displayScore.innerText = `${score}`;
-                inputNum.value = "";
-            } else if (inputNum.valueAsNumber > randomNum) {
-                displayMessage.innerText = "This Number comes after the Guess Number";
-                console.log(inputNum.valueAsNumber);
-                displayScore.innerText = `${score}`;
-                inputNum.value = "";
-            }
-        }
-    } else {
-        score--;
-        displayMessage.innerText = "Please enter your number between 0 to 100 and then check your answer!";
-        console.log(inputNum.valueAsNumber);
-        inputNum.value = "";
-    }}else{
-        displayMessage.innerText = "Ohh you fail the challenge!"
-        setTimeout(() =>{
-            location.reload();
+  if (score !== 0) {
+    if (inputNum.valueAsNumber >= 0 && inputNum.valueAsNumber <= 100) {
+      if (inputNum.valueAsNumber === randomNum) {
+        displayMessage.innerText = `Hurray! You got The Number ${randomNum} and your Score is ${score}`;
+        displayScore.innerText = `${score}`;
+        setTimeout(() => {
+          location.reload();
         }, 5000);
+      } else {
+        score--;
+        if (
+          inputNum.valueAsNumber < randomNum &&
+          Math.abs(inputNum.valueAsNumber - randomNum) <= 3
+        ) {
+          displayMessage.innerText = "You are very close to the Number";
+          console.log(inputNum.valueAsNumber);
+          displayScore.innerText = `${score}`;
+          inputNum.value = "";
+        } else if (inputNum.valueAsNumber < randomNum) {
+          displayMessage.innerText =
+            "This Number comes before the Guess Number";
+          console.log(inputNum.valueAsNumber);
+          displayScore.innerText = `${score}`;
+          inputNum.value = "";
+        } else if (inputNum.valueAsNumber > randomNum) {
+          displayMessage.innerText = "This Number comes after the Guess Number";
+          console.log(inputNum.valueAsNumber);
+          displayScore.innerText = `${score}`;
+          inputNum.value = "";
+        }
+      }
+    } else {
+      score--;
+      displayMessage.innerText =
+        "Please enter your number between 0 to 100 and then check your answer!";
+      console.log(inputNum.valueAsNumber);
+      inputNum.value = "";
     }
-}
+  } else {
+    displayMessage.innerText = "Ohh you fail the challenge!";
+    setTimeout(() => {
+      location.reload();
+    }, 5000);
+  }
+};
 
 // This Function Calls Out When Reset Button Click
-const resetGame = () =>{
-    location.reload(); 
-}
-
-
+const resetGame = () => {
+  location.reload();
+};
